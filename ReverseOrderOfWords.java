c06f919221916880b213b415f6766c947da82662
Nagalakshmi R <naganandhini475@gmail.com>
2026-06-30 18:11:27 +0530

    Create ReverseOrderOfWords.java

---
+++ ReverseOrderOfWords.java
@@ -0,0 +1,8 @@
+class ReverseOrderOfWords {
+    public static void main(String[] args){
+        String s = "I Like Reading";
+        String arr[] = s.split(" ");
+        for(int i = arr.length-1;i>=0;i--){
+            System.out.print(arr[i] +" ");}
+    }
+}
