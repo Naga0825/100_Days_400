5abee8c95b0e5eab01e6b131f02f7968eeaa3219
Nagalakshmi R <naganandhini475@gmail.com>
2026-07-08 22:13:39 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class ReverseArray{
+    public static int[] reverse(int[] arr){
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int[] result = reverse(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
